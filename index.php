69e75521ca31facba45d4858804f52f69b1f12ed
Zhyraa <roralolxd@gmail.com>
2022-07-17 18:27:20 +0700

    start

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'header.php' ?>
+<div class="d-flex flex-row justify-content-center my-5">
+   <div class="card mx-3" style="width: 30rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+   </div>
+
+   <div class="card mx-3 " style="width: 30rem;">
+      <iframe src="https://www.youtube.com/embed/BXpjwL1jxQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+   </div>
+
+   <div class="card mx-3 " style="width: 30rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+   </div>
+</div>
+<?php include 'footer.php' ?>
+a
