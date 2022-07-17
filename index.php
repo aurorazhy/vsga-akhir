d428c2e919f2cabb68b0f689acbb982c1b1ac059
Zhyraa <roralolxd@gmail.com>
2022-07-17 19:25:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <?php include 'header.php' ?>
-<div class="d-flex flex-row justify-content-center my-5">
-   <div class="card mx-3" style="width: 30rem;">
-      <img src="..." class="card-img-top" alt="...">
+<div class="d-lg-flex flex-row my-5">
+   <div class="card me-5 my-5" style="width: 50rem;">
+      <iframe class="p-3" height="240" src="https://www.youtube.com/embed/-Pu_IPSGRi0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
+         <h4 class="card-title">Pantai Muara Beting</h4>
+         <h6 class="card-title">Rp 40.000/orang </h6>
+         <p class="card-text">Pemandangan pantai di Bekasi ini cukup memesona dan dapat menghilangkan stres. Pasirnya berwarna agak coklat dan suasana pantainya begitu segar. Tak hanya itu, pemandangan sekitar pantai pun menenangkan dan jauh dari hiruk pikuk keramaian kota.</p>
+         <a href="#" class="btn btn-secondary">Lihat Detail</a>
       </div>
    </div>
 
-   <div class="card mx-3 " style="width: 30rem;">
-      <iframe src="https://www.youtube.com/embed/BXpjwL1jxQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   <div class="card me-5 my-5 " style="width: 50rem;">
+      <iframe class="p-3" height="240" src="https://www.youtube.com/embed/BXpjwL1jxQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
+         <h4 class="card-title">Go Wet! All Out!</h4>
+         <h6 class="card-title">Rp 250.000/orang </h6>
+         <p class="card-text">Explore Go! Wet waterpark di Grand Wisata Bekasi. Rasakan pengalaman bermain air bersama teman atau keluarga di berbagai wahana yang ada di Go! Wet.</p>
+         <a href="#" class="btn btn-secondary">Lihat Detail</a>
       </div>
    </div>
 
-   <div class="card mx-3 " style="width: 30rem;">
-      <img src="..." class="card-img-top" alt="...">
+   <div class="card  my-5 " style="width: 50rem;">
+      <iframe class="p-3" height="240" src="https://www.youtube.com/embed/kUzI-JlUyS0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
+         <h4 class="card-title">Gedung Juang 45</h4>
+         <h6 class="card-title">Rp 10.000/orang </h6>
+         <p class="card-text">Gedung Juang 45 Bekasi menjadi museum yang menyimpan banyak sejarah yang dapat dipelajari dengan seru, karena penjelasan sejarahnya yang diperlihatkan secara digital.</p>
+         <a href="#" class="btn btn-secondary">Lihat Detail</a>
       </div>
    </div>
 </div>
