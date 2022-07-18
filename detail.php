ef8d23c9589e53a7fa38bf3456986d8f3fbedc46
Zhyraa <roralolxd@gmail.com>
2022-07-19 02:05:07 +0700

    done

--- detail.php
+++ detail.php
@@ -4,45 +4,52 @@ $id = $_GET['id']; ?>
 <div class="d-flex flex-row my-5">
    <div class="d-flex flex-column my-5">
       <div class="col-12">
-         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
             <div class="carousel-indicators">
-               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" aria-current="true" aria-label="Slide 1"></button>
-               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
             </div>
-            <?php $data = mysqli_query($con, "SELECT * FROM tempat WHERE id = '$id'");
-            while ($t = mysqli_fetch_array($data)) { ?>
-               <div class="carousel-inner">
+            <div class="carousel-inner">
+               <?php $data = mysqli_query($con, "SELECT * FROM tempat WHERE id = '$id'");
+               while ($t = mysqli_fetch_array($data)) { ?>
                   <div class="carousel-item active">
-                     <img src="images/<?= $t['gambar1'] ?>" class="d-block w-100" alt="...">
+                     <img src="images/<?= $t['gambar1'] ?>" class="d-block w-100" height="250" alt="...">
                      <div class="carousel-caption d-none d-md-block">
-                        <p class="fs-6">Rp <?= $t['harga'] ?>/orang</p>
+                        <p>Rp <?= $t['harga'] ?>/orang</p>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <img src="images/<?= $t['gambar2'] ?>" class="d-block w-100" height="250" alt="...">
+                     <div class="carousel-caption d-none d-md-block">
+                        <p>Rp <?= $t['harga'] ?>/orang</p>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <img src="images/<?= $t['gambar3'] ?>" class="d-block w-100" height="250" alt="...">
+                     <div class="carousel-caption d-none d-md-block">
+                        <p>Rp <?= $t['harga'] ?>/orang</p>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <img src="images/<?= $t['gambar4'] ?>" class="d-block w-100" height="250" alt="...">
+                     <div class="carousel-caption d-none d-md-block">
+                        <p>Rp <?= $t['harga'] ?>/orang</p>
                      </div>
                   </div>
                <?php } ?>
-               <div class="carousel-item">
-                  <img src="a.png" class="d-block w-100" alt="...">
-                  <div class="carousel-caption d-none d-md-block">
-                     <h5>Rp 250.000/orang</h5>
-                  </div>
-               </div>
-               <div class="carousel-item">
-                  <img src="a.png" class="d-block w-100" alt="...">
-                  <div class="carousel-caption d-none d-md-block">
-                     <h5>Rp 250.000/orang</h5>
-                  </div>
-               </div>
-               </div>
-               <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-               </button>
-               <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-               </button>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+            </button>
          </div>
+
          <div class="text">
             <?php $data = mysqli_query($con, "SELECT * FROM tempat WHERE id = '$id'");
             while ($t = mysqli_fetch_array($data)) { ?>
